81ebbca8e8ebc0557f5c7ecee3e6cb7a28032796
Tieu92 <alisonmat@gmail.com>
2026-06-22 23:02:00 +0200

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   output: "static",
-  base: "/web-accte",
-  trailingSlash: 'always',
+  site: "https://www.accte.org",
+  trailingSlash: "always",
 });
